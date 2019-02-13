1fc68120b236aa29312af4b80a2a1852562fbe7d
synycboom <synycboom@gmail.com>
2019-02-13 17:12:09 +0700

    Use cookie instead of token

--- src/apis/AuthService.js
+++ src/apis/AuthService.js
@@ -1,21 +1,29 @@
 import BaseService from './BaseService';
+import { getCookie } from '../utils/cookie';
 
 class AuthService extends BaseService {
   isAuthenticated() {
-    return !!localStorage.getItem('token');
+    return getCookie('logged_in') === 'true';
+    // return !!localStorage.getItem('token');
   }
 
   authenticate(username, password) {
-    return this.client.post('/admin-api/v1.0/get-token/', { username, password })
-      .then(({data: { token }}) => {
-        localStorage.setItem('token', token);
+    return this.client
+      .post('/api/v1.0/login/', { username, password })
+      .then(({ data: { token } }) => {
+        // localStorage.setItem('token', token);
       })
-      .catch(this.handleError)
+      .catch(this.handleError);
   }
-  
+
   signout() {
-    localStorage.removeItem('token');
-    return Promise.resolve();
+    return this.client
+      .post('/api/v1.0/logout/')
+      .then(data => {
+        // localStorage.removeItem('token');
+        return data;
+      })
+      .catch(this.handleError);
   }
 }
 
--- src/apis/BaseService.js
+++ src/apis/BaseService.js
@@ -4,31 +4,36 @@ import axios from 'axios';
 export default class BaseService {
   constructor() {
     this.client = axios.create({
-      baseURL: baseUrl,
+      baseURL: baseUrl
     });
-    this.client.interceptors.request.use(function (config) {
-      const token = localStorage.getItem('token');
+    this.client.interceptors.request.use(
+      function(config) {
+        // Use cooke instead of token
+        // const token = localStorage.getItem('token');
 
-      if (token) {
-        config.headers.Authorization = `Token ${token}`;
+        // if (token) {
+        //   config.headers.Authorization = `Token ${token}`;
+        // }
+        config.withCredentials = true;
+        return config;
+      },
+      function(error) {
+        return Promise.reject(error);
       }
-      return config;
-    }, function (error) {
-      return Promise.reject(error);
-    });
+    );
   }
 
   handleError = error => {
     if (error.response) {
       return Promise.reject({
-        ...error.response.data, 
+        ...error.response.data,
         __meta__: {
           status: error.response.status
-        },
+        }
       });
     } else {
       console.log(error);
-      return Promise.reject({detail: 'Something Error, please check logs.'});
+      return Promise.reject({ detail: 'Something Error, please check logs.' });
     }
-  }
+  };
 }
